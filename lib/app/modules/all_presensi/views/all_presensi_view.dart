1c490d0cf963a109d2e1fc0cc723ada9e07a9575
unuStudents <nanda.19572011009@students.unugha.ac.id>
2022-10-06 20:17:43 +0700

    all detail presence

--- lib/app/modules/all_presensi/views/all_presensi_view.dart
+++ lib/app/modules/all_presensi/views/all_presensi_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -29,71 +30,103 @@ class AllPresensiView extends GetView<AllPresensiController> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.all(15),
-              itemCount: 15,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.only(bottom: 10),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.amber,
-                    child: InkWell(
-                      onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI),
-                      borderRadius: BorderRadius.circular(10),
-                      child: Container(
-                        // margin: EdgeInsets.only(bottom: 15),
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          // color: Colors.amber,
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "Masuk",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  "${DateFormat.yMMMEd().format(DateTime.now())}",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              "${DateFormat.jms().format(DateTime.now())}",
-                            ),
-                            SizedBox(
-                              height: 10,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  "Keluar",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                                Text(
-                                  "${DateFormat.yMMMEd().format(DateTime.now())}",
-                                  style: TextStyle(fontWeight: FontWeight.bold),
-                                ),
-                              ],
-                            ),
-                            Text(
-                              "${DateFormat.jms().format(DateTime.now())}",
-                            ),
-                          ],
-                        ),
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: controller.streamAllPresence(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  }
+
+                  if (snapshot.data?.docs.length == 0 ||
+                      snapshot.data == null) {
+                    return SizedBox(
+                      height: 100,
+                      child: Center(
+                        child: Text(
+                            "Belum ada histori absen sejak 5 hari yang lalu"),
                       ),
-                    ),
-                  ),
-                );
-              },
-            ),
+                    );
+                  }
+
+                  return ListView.builder(
+                    padding: EdgeInsets.all(15),
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      Map<String, dynamic> data =
+                          snapshot.data!.docs.reversed.toList()[index].data();
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Material(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.amber,
+                          child: InkWell(
+                            onTap: () => Get.toNamed(Routes.DETAIL_PRESENSI,
+                                arguments: data),
+                            borderRadius: BorderRadius.circular(10),
+                            child: Container(
+                              // margin: EdgeInsets.only(bottom: 15),
+                              padding: EdgeInsets.all(20),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                // color: Colors.amber,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "Masuk",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                        "${DateFormat.yMMMEd().format(DateTime.parse(data['date']))}",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                  Text(
+                                    data['masuk']!['date'] == null
+                                        ? "- - -"
+                                        : "${DateFormat.jms().format(DateTime.parse(data['masuk']?['date']))}",
+                                  ),
+                                  SizedBox(
+                                    height: 10,
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        "Keluar",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      // Text(
+                                      //   "${DateFormat.yMMMEd().format(DateTime.now())}",
+                                      //   style: TextStyle(
+                                      //       fontWeight: FontWeight.bold),
+                                      // ),
+                                    ],
+                                  ),
+                                  Text(
+                                    data['keluar']?['date'] == null
+                                        ? "- - -"
+                                        : "${DateFormat.jms().format(DateTime.parse(data['keluar']?['date']))}",
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }),
           ),
         ],
       ),
